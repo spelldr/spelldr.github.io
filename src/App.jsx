a1636581f5ff286357fdcf62d10a30a70638c8af
spelldr <71286761+spelldr@users.noreply.github.com>
2023-06-11 14:27:30 -0400

    map!

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import data from './data/resume.json'
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <CardGroup>
-            <Card>
-              {data.map((resume) => (
-                <div className='a' key = {resume.id}>
-                  <h2>{resume.data.contact.name}</h2>
-                  {console.log(data)}
-                </div>
-              ))}
+    <Container className='container'>
+      <CardGroup>
+        {data.map((resume) => (
+          <>
+            <Card className='contact card'>
+              <h2>{resume.contact.name}</h2>
+              <h5>{resume.content.tagline}</h5>
+              <p>
+                {JSON.stringify(resume.contact)}
+              </p>
             </Card>
-          </CardGroup>
-        </Col>
-      </Row>
+            <Card className='hobbies card'>
+              <h2>Hobbies</h2>
+              <ul>
+              {
+                resume.content.hobbies.map((hobby) =>
+                  <li>
+                    {hobby}
+                  </li>
+                )
+              }
+              </ul>
+            </Card>
+          </>
+        ))}
+      </CardGroup>
     </Container>
   );
 }
